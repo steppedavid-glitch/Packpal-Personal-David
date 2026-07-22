72cdcab64098898efcba70725b39c1e861d74705
steppedavid-glitch <steppe.david@gmail.com>
2026-07-22 13:53:15 +0200

    Create defaultPackingPersonal.js

---
+++ src/defaultPackingPersonal.js
@@ -0,0 +1,655 @@
+import defaultPacking from "../utils/defaultPacking";
+import uuid from "../utils/uuid";
+
+export default function defaultPackingPersonal() {
+
+    const packing = defaultPacking();
+
+    packing.clothing = [
+
+        {
+            id: uuid(),
+            name: "Zonnebrillen",
+            quantity: 2,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Leesbrillen",
+            quantity: 2,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Outfits",
+            quantity: 7,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Ondergoed",
+            quantity: 14,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Slaapshortje Wendy",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Washandjes",
+            quantity: 2,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Extra handtas",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Zwarte slippers Wendy",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Beige slippers Wendy",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Schoenen Wendy",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Geklede slippers Wendy",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Sportgerief",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Petje David",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Slippers David",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Zwemzak",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Bikini's Wendy",
+            quantity: 3,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Zwemshort David",
+            quantity: 2,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Snorkel en bril",
+            quantity: 2,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Zeeschoenen Wendy",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Zeeschoenen David",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Handdoeken zwembad",
+            quantity: 2,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Ibiza handdoek",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Waszakken",
+            quantity: 2,
+            completed: false
+        }
+
+    ];
+
+    packing.toiletries = [
+
+        {
+            id: uuid(),
+            name: "Tadaa Spray",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Lippen SPF30",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Tandenborstels",
+            quantity: 2,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Tandpasta",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Mondwater",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Wenkbrauwverf",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Oorstokjes",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Anti Insect",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Après Insect",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Zonnecrème SPF30",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Zonnecrème SPF50",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Après Soleil",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Micellair Water",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Watjes",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Lippenbalsem",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Vaseline / Anti Friction",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Cicaplast",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Inlegkruisjes",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Scottex doekjes",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Brillendoekjes",
+            quantity: 1,
+            completed: false
+        }
+
+    ];
+    packing.medicine = [
+
+        {
+            id: uuid(),
+            name: "Daflon",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Pantomed",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Levocetirizine",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Brufen",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Neusspray",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Pil",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Rosuvastatine",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Motilium",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Riopan",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Rhynofebryl",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Paracetamol",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Strepsils",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Thermometer",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Panacort",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Pleisters",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Ontsmetting",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Schaartje",
+            quantity: 1,
+            completed: false
+        }
+
+    ];
+
+    packing.electronics = [
+
+        {
+            id: uuid(),
+            name: "iPad",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "iPad lader",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Selfiestick",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Drone",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Koffiezet",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Lader David",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Lader Wendy",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Oortjes David",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Oortjes Wendy",
+            quantity: 1,
+            completed: false
+        }
+
+    ];
+      packing.documents = [
+
+        {
+            id: uuid(),
+            name: "Paspoorten",
+            quantity: 2,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Reisdocumenten",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Bankkaarten",
+            quantity: 2,
+            completed: false
+        }
+
+    ];
+
+    packing.other = [
+
+        {
+            id: uuid(),
+            name: "Speculoos",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Muntthee",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Koffie",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Boek",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Yahtzee",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Rummikub",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Lunchpakket",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Drinkflessen",
+            quantity: 2,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Downloads controleren",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Haardroger",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Stijltang",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Schmink",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Haarborstels",
+            quantity: 2,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Nagelknipper",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Paraplu",
+            quantity: 2,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Rugzak",
+            quantity: 1,
+            completed: false
+        },
+
+        {
+            id: uuid(),
+            name: "Herbruikbare boodschappentas",
+            quantity: 2,
+            completed: false
+        }
+
+    ];
+  
+    return packing;
+
+}
